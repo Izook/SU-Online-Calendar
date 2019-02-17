481f31eb51e87cd56efb49c95452856bbbddec2a
Audrey Kan <audreykan@Audreys-Air-2.seattleu.edu>
2019-02-17 12:28:47 -0800

    Changes to scripts.js

--- scripts.js
+++ scripts.js
@@ -1,19 +1,6 @@
-function getLogin() {
-  var username = document.getElementById('text').type
-  var pass = document.getElementById('password').type
-  //do some stuff
-}
-
-//TODO how to hide and show?
-function switchView() {
-  $("#submit").click(function() {
-    $("login-title").hide();
-    $("login-page").hide();
-    $("calendar-title").show();
-    $("schedule-page").show();
-  });
-}
-
-function showSchedule() {
-  var x = document.getElementById('quarters').value
-}
+$("#submitHide").click(function() {
+    $("#login-title").hide();
+    $("#login-page").hide();
+    $("#calendar-title").show();
+    $("#schedule-page").show();
+});
